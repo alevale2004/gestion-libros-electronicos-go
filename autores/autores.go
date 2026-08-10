e9836d4a60290b45e5627b9883477ee0890be4a0
alevale2004 <aleascanio@outlook.com>
2026-08-09 20:52:44 -0500

    Implementar estructura Autor
    Added Autor struct and associated methods for managing author details.

--- autores/autores.go
+++ autores/autores.go
@@ -1,1 +1,63 @@
+package autores
 
+import (
+	"errors"
+	"strings"
+)
+
+type Autor struct {
+	id           int
+	nombre       string
+	nacionalidad string
+	biografia    string
+}
+
+func NewAutor(id int, nombre, nacionalidad, biografia string) (*Autor, error) {
+	if id <= 0 {
+		return nil, errors.New("el ID del autor debe ser mayor que cero")
+	}
+
+	if strings.TrimSpace(nombre) == "" {
+		return nil, errors.New("el nombre del autor no puede estar vacío")
+	}
+
+	return &Autor{
+		id:           id,
+		nombre:       nombre,
+		nacionalidad: nacionalidad,
+		biografia:    biografia,
+	}, nil
+}
+
+func (a Autor) GetID() int {
+	return a.id
+}
+
+func (a Autor) GetNombre() string {
+	return a.nombre
+}
+
+func (a Autor) GetNacionalidad() string {
+	return a.nacionalidad
+}
+
+func (a Autor) GetBiografia() string {
+	return a.biografia
+}
+
+func (a *Autor) SetNombre(nombre string) error {
+	if strings.TrimSpace(nombre) == "" {
+		return errors.New("el nombre no puede estar vacío")
+	}
+
+	a.nombre = nombre
+	return nil
+}
+
+func (a *Autor) SetNacionalidad(nacionalidad string) {
+	a.nacionalidad = nacionalidad
+}
+
+func (a *Autor) SetBiografia(biografia string) {
+	a.biografia = biografia
+}
